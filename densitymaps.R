b0eba5c474a6783f749581373f2ec301e0a327a1
Sachit Mahajan <sachitmahajan90@gmail.com>
2023-05-08 11:00:54 +0200

    Create densitymaps.R

---
+++ densitymaps.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(ggmap)
+library(osmdata)
+library(sp)
+library(spatstat)
+library(dplyr)
+library(spdep)
+library(tidyverse)
+library(cluster)
+library(spNetwork)
+data <- read.csv("data.csv") #the data file includes data of the the amenities that are being analysed
+data <- data %>% distinct(lon, lat)
+map <- get_map( getbb('City'), source="stamen") #Change the name of the city
+mapPoints <- ggmap(map)
+mapPoints + stat_density2d(aes(fill = ..level..), alpha = .5, h = .02, geom = "polygon", data = data) + scale_fill_viridis_c() + theme(legend.position = 'center'+ theme(text = element_text(size=18)))
